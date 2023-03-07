cc9a72445abba7b123aa90981ee67a628ca145a6
valyalshikovd <770vda@mail.ru>
2023-03-08 01:28:50 +0300

    08.03.23

--- src/Task2/Main.java
+++ src/Task2/Main.java
@@ -1,83 +1,16 @@
 package Task2;
 
+import java.util.Arrays;
 import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) {
-//        MyLinkedList<Integer> list1 = new MyLinkedList<>(new LinkedListNode<>(10));
-//        list1.add(1);
-//        list1.add(23);
-//        list1.add(54);
-//        list1.add(432);
-//        list1.add(433);
-//
-//        new MyFrame();
-//
-//        MyLinkedList<Integer> list2 = new MyLinkedList<>(new LinkedListNode<>(11));
-//       list2.add(4);
-//        list2.add(32);
-//        list2.add(450);
-//        list2.add(451);
-//        unity(list1, list2).printLinked();
-//    }
-//    public static MyLinkedList unity(MyLinkedList<Integer> list1, MyLinkedList<Integer> list2 ){
-//        MyLinkedList<Integer> res = new MyLinkedList<>();
-//        LinkedListNode currentNode1 = list1.getFirstElement();
-//        LinkedListNode currentNode2 = list2.getFirstElement();
-//        while(!Objects.equals(currentNode1.getLink(), null) && !Objects.equals(currentNode2.getLink(), null)){
-//            if((Integer) currentNode1.getValue() < (Integer) currentNode2.getValue()){
-//                res.add(currentNode1.getValue());
-//                System.out.println("антихайп");
-//                res.printLinked();
-//                boolean flag = true;
-//                while (flag && !Objects.equals(currentNode1.getLink(), null) && !Objects.equals(currentNode2.getLink(), null)){
-//                    if((Integer) currentNode1.getLink().getValue() < (Integer) currentNode2.getValue()){
-//                        res.add(currentNode1.getLink().getValue());
-//                        res.printLinked();
-//                        currentNode1 = currentNode1.getLink();
-//                    }else{
-//                        flag = false;
-//                        currentNode1 = currentNode1.getLink();
-//                        res.add(currentNode2.getValue());
-//                        res.printLinked();
-//                        currentNode2 = currentNode2.getLink();
-//                    }
-//
-//                }
-//            }else{
-//                res.add(currentNode2.getValue());
-//                System.out.println("антихайп2");
-//                res.printLinked();
-//
-//                boolean flag = true;
-//                while (flag && !Objects.equals(currentNode1.getLink(), null) && !Objects.equals(currentNode2.getLink(), null)){
-//                    if((Integer) currentNode2.getLink().getValue() < (Integer) currentNode1.getValue()){
-//                        res.add(currentNode2.getLink().getValue());
-//                        res.printLinked();
-//                        currentNode2 = currentNode2.getLink();
-//                    }else{
-//                        flag = false;
-//                        currentNode2 = currentNode2.getLink();
-//                        res.add(currentNode1.getValue());
-//                        res.printLinked();
-//                        currentNode1 = currentNode1.getLink();
-//                    }
-//
-//                }
-//            }
-//        }
-//        if (Objects.equals(currentNode1.getLink(), null)) {
-//            while(currentNode2.nextExist()){
-//                res.add(currentNode2.getValue());
-//                currentNode2 = currentNode2.getLink();
-//            }
-//        }
-//        if (Objects.equals(currentNode2.getLink(), null)) {
-//            while(currentNode1.nextExist()){
-//                res.add(currentNode1.getValue());
-//                currentNode1 = currentNode1.getLink();
-//            }
-//        }
-//        return res;
+        MyLinkedList<Integer> list = new MyLinkedList<>(13);
+        list.add(32);
+        list.add(43);
+        list.set(1, 111);
+        System.out.println(Arrays.toString(list.toArray()));
+        list.remove(0);
+        System.out.println(Arrays.toString(list.toArray()));
     }
 }
--- src/Task2/MyLinkedList.java
+++ src/Task2/MyLinkedList.java
@@ -1,7 +1,10 @@
 package Task2;
 
-public class MyLinkedList<T> {
-    private class MyLinkedListNode<T>{
+import java.util.Collection;
+import java.util.Iterator;
+
+public class MyLinkedList<T> implements Collection<T> {
+    private class MyLinkedListNode<T> {
         private T value;
         private int index;
         private MyLinkedListNode<T> next;
@@ -36,13 +39,158 @@ public class MyLinkedList<T> {
             this.next = next;
         }
         private boolean nextExist(){
-            return next==null;
+            return next != null;
         }
+
+        @Override
+        public String toString() {
+            return "MyLinkedListNode{" +
+                    "value=" + value +
+                    ", index=" + index +
+                    ", next=" + next +
+                    '}';
+        }
+
     }
 
     private MyLinkedListNode<T> firstElementh;
     private MyLinkedListNode<T> lastElementh;
     private int size;
 
+    public MyLinkedList() {
+        this.firstElementh = null;
+        this.lastElementh = null;
+        this.size = 0;
+    }
+    public MyLinkedList(T value) {
+        MyLinkedListNode<T> tmp = new MyLinkedListNode<>(value, 0, null);
+        this.firstElementh = tmp;
+        this.lastElementh = tmp;
+        this.size = 1;
+    }
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size==0;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        MyLinkedListNode<T> current = firstElementh;
+        while (current.nextExist()){
+            if(current.getNext().getValue().equals(o)){
+                return true;
+            }
+            current = current.getNext();
+        }
+        return false;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        Object[] res = new Object[size];
+        MyLinkedListNode<T> current = firstElementh;
+        int counter = -1;
+        while (current != null){
+            counter++;
+            res[counter] = current.getValue();
+            current = current.getNext();
+        }
+        return res;
+    }
+
+    @Override
+    public <T1> T1[] toArray(T1[] a) {
+        return null;
+    }
+
+    @Override
+    public boolean add(T value){
+        if(firstElementh == null){
+            MyLinkedListNode<T> tmp = new MyLinkedListNode<>(value, 0, null);
+            firstElementh = tmp;
+            lastElementh = tmp;
+            this.size = 1;
+        }else{
+            MyLinkedListNode<T> current = firstElementh;
+            while (current.nextExist()){
+                current = current.getNext();
+            }
+            current.setNext(new MyLinkedListNode<>(value, current.getIndex() + 1, null));
+            lastElementh = current.getNext();
+            size++;
+        }
+        return true;
+    }
+    public T get(int index){
+        MyLinkedListNode<T> current = firstElementh;
+        for (int i = 0; i < index + 1; i++){
+            current = current.getNext();
+        }
+        return current.getValue();
+    }
+    public void set(int index, T value){
+        MyLinkedListNode<T> current = firstElementh;
+        for (int i = 0; i < index; i++){
+            current = current.getNext();
+        }
+        current.setValue(value);
+    }
+    public void remove(int index){
+        if(index == 0){
+            firstElementh = firstElementh.getNext();
+            size --;
+            return;
+        }
+        MyLinkedListNode<T> current = firstElementh;
+        for (int i = 0; i < index - 1; i++){
+            current = current.getNext();
+        }
+        if(current.getNext().nextExist()){
+            current.setNext(current.getNext().getNext());
+        }else{
+            current.setNext(null);
+            lastElementh = current;
+        }
+        size --;
+    }
+    @Override
+    public boolean remove(Object o) {
+
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
 
 }
