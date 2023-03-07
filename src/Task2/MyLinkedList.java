3626da797b0a608da3ab0905ba1a29a60e33bf44
valyalshikovd <770vda@mail.ru>
2023-03-07 21:56:18 +0300

    07.03.2023№2

--- src/Task2/MyLinkedList.java
+++ src/Task2/MyLinkedList.java
@@ -1,18 +1,10 @@
 package Task2;
 
-import java.lang.reflect.Array;
-import java.util.Iterator;
-import java.util.Objects;
-import java.util.Spliterator;
-import java.util.function.Consumer;
-
-public class MyLinkedList<T> implements Iterable<T> {
-
-
-    public class LinkedListNode<T> {
+public class MyLinkedList<T> {
+    private class MyLinkedListNode<T>{
         private T value;
         private int index;
-        private LinkedListNode<T> link;
+        private MyLinkedListNode<T> next;
 
         public T getValue() {
             return value;
@@ -22,131 +14,35 @@ public class MyLinkedList<T> implements Iterable<T> {
             this.value = value;
         }
 
-        public LinkedListNode<T> getLink() {
-            if (link != null) {
-                return link;
-            }
-            return null;
+        public int getIndex() {
+            return index;
         }
 
-        public void setLink(LinkedListNode<T> link) {
-            this.link = link;
+        public void setIndex(int index) {
+            this.index = index;
         }
 
-        public LinkedListNode(T value) {
+        public MyLinkedListNode<T> getNext() {
+            return next;
+        }
+
+        public void setNext(MyLinkedListNode<T> next) {
+            this.next = next;
+        }
+
+        public MyLinkedListNode(T value, int index, MyLinkedListNode<T> next) {
             this.value = value;
-            this.link = null;
-
+            this.index = index;
+            this.next = next;
         }
-
-        public boolean nextExist() {
-            return !Objects.equals(link, null);
-        }
-    }
-
-    private LinkedListNode<T> firstElement;
-    private int counter;
-
-    public MyLinkedList(T value) {
-        this.firstElement = new LinkedListNode<T>(value);
-        this.counter = 0;
-    }
-
-    public MyLinkedList() {
-        this.firstElement = null;
-        this.counter = -1;
-    }
-
-    public LinkedListNode<T> getFirstElement() {
-        return firstElement;
-    }
-
-    public void add(T value) {
-        if (firstElement == null) {
-            firstElement = new LinkedListNode<T>(value);
-            counter++;
-        }
-        LinkedListNode<T> temp = new LinkedListNode<>(value);
-        LinkedListNode<T> elementh = firstElement;
-        while (true) {
-            if (elementh.getLink() == null) {
-                elementh.setLink(temp);
-                counter++;
-                break;
-            } else {
-                elementh = elementh.getLink();
-            }
+        private boolean nextExist(){
+            return next==null;
         }
     }
 
-    public void get(T elem) {
-        LinkedListNode<T> elementh = firstElement;
-        while (true) {
-            if (elementh.getLink() == null) {
-                elementh.setLink(temp);
-                counter++;
-                break;
-            } else {
-                elementh = elementh.getLink();
-            }
-        }
-    }
+    private MyLinkedListNode<T> firstElementh;
+    private MyLinkedListNode<T> lastElementh;
+    private int size;
 
-    public void printLinked() {
-        LinkedListNode<T> tmp = this.firstElement;
-        while (tmp != null) {
-            System.out.print(tmp.getValue() + " ");
-            tmp = tmp.getLink();
-        }
-        System.out.println();
-    }
 
-    public int size() {
-        return counter;
-    }
-
-    public void remove(T elem) {
-        LinkedListNode<T> elementh = this.getFirstElement();
-        while (true) {
-            if (elementh.getLink().getValue() == elem) {
-                elementh.setLink(elementh.getLink().getLink());
-                counter--;
-                return;
-            } else {
-                elementh = elementh.getLink();
-            }
-        }
-    }
-
-    public T[] asArray() {
-        T[] res = (T[]) new Array[this.size()];
-        LinkedListNode<T> tmp = this.firstElement;
-        for (int i = 0; i < this.size(); i++) {
-            res[i] = tmp.getValue();
-            tmp = tmp.getLink();
-        }
-        return res;
-    }
-
-    @Override
-    public Iterator<T> iterator() {
-
-        class LinkedListIterator implements Iterator<T> {
-
-            LinkedListNode<T> curr = firstElement;
-
-            @Override
-            public boolean hasNext() {
-                return curr != null;
-            }
-
-            @Override
-            public T next() {
-                T value = curr.value;
-                curr = curr.link;
-                return value;
-            }
-        }
-        return new LinkedListIterator();
-    }
 }
